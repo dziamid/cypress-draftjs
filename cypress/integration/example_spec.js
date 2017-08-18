32c397442ec95a3e677f7899dd13b2d78008e4c1
Dziamid Zayankouski <dziamid@gmail.com>
2017-08-18 13:31:09 +0300

    added expectation

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,11 +1,13 @@
 
 describe('React RTE', function(){
 
-  it('should be possible to type in editor', function(){
+  it('i cannot simply type', function () {
 
     cy
         .visit('https://react-rte.org/demo')
         .get('[contenteditable]').eq(0).type('I am typing')
-  })
+        .get('textarea').eq(0).contains('I am typing')
+  });
+
 
 })
